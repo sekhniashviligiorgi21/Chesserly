f1bc255b3676870a9aac4890b0aa9d8615b41fa1
GS21 <sekhniashviligiorgi21@gmail.com>
2026-09-14 10:36:57 +0400

    Add files via upload

--- src/assets/Title.vue
+++ src/assets/Title.vue
@@ -85,7 +85,6 @@
         class="btn tooltip-btn"
         :style="{ background: `linear-gradient(${bgColor('puzzles')})` }"
         @click="puzzlesClick()"
-        disabled
         data-tooltip="Coming soon..."
       >
         🧩 Puzzles
